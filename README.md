43d5d0f14823bb190b8e94a5c2f6be644a117875
Youngchan Kim <ovekyc@gmail.com>
2016-12-02 01:46:52 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# mac_save_selected_text
-Save text with shortcut in mac
+# Text Logger for mac
+
+Save selected text with shortcut in mac
+
+Tested only for English
+
+## How to use it
+
+* Select text what you want to save
+* Press shortcut
+* You can see saved contents from file
+
+## Install
+
+1. Open Automator
+2. Choose service
+3. Search for the Run Javascript action, and add it to your Service
+4. Paste script in this repo & set path of log file
+5. Save service
+6. Open the Keyboard preferences of the System Preferences pane
+7. Select Shortcuts tab and find the service
+8. Bind shortcut which you want. (Recommand Control + Command + Letter)
---
+++ text_logger.js
@@ -0,0 +1,30 @@
+var app = Application.currentApplication();
+app.includeStandardAdditions = true;
+
+function run(input, parameters) {
+  var content = input[0];
+  writeTextToFile(content);
+}
+
+function writeTextToFile(text, file = '/log-file-path/log-file-name.txt', overwriteExistingContent = false) {
+  text += '\n ';
+  try {
+    var fileString = file.toString();
+    var openedFile = app.openForAccess(Path(fileString), { writePermission: true });
+ 
+    if (overwriteExistingContent) {
+      app.setEof(openedFile, { to: 0 });
+    }
+
+    app.write(text, { to: openedFile, startingAt: app.getEof(openedFile) });
+    app.closeAccess(openedFile);
+    return true;
+  } catch(error) {
+    try {
+      app.closeAccess(file);
+    } catch(error) {
+      console.log(`Couldn't close file: ${error}`);
+    }
+    return false;
+  }
+}
